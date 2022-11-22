06c834e3d646e046a0b0cf77cb9dad03bd5fe198
Pt0819 <2471408797@qq.com>
2022-11-22 16:33:03 +0800

    Initial commit

---
+++ MyBus_backand/src/main/java/com/example/MyBus_backand/MyBusBackandApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyBus_backand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBusBackandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBusBackandApplication.class, args);
+	}
+
+}
---
+++ MyBus_backand/src/test/java/com/example/MyBus_backand/MyBusBackandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyBus_backand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBusBackandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
