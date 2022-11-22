8337521b8555eb07b1663bc7e3d2b005f62b1124
Pt0819 <2471408797@qq.com>
2022-11-22 16:33:03 +0800

    Initial commit

--- MyBus_backand/src/main/java/com/example/MyBus_backand/MyBusBackandApplication.java
+++ MyBus_backand/src/main/java/com/example/MyBus_backand/MyBusBackandApplication.java
@@ -2,8 +2,10 @@ package com.example.MyBus_backand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class MyBusBackandApplication {
 
 	public static void main(String[] args) {
