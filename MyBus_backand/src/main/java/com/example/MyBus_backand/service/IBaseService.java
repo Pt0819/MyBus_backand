98c7fa315a07be9976f06c5c520fce9e046cbee2
Pt0819 <2471408797@qq.com>
2022-11-23 15:28:06 +0800

    Pt0819

---
+++ MyBus_backand/src/main/java/com/example/MyBus_backand/service/IBaseService.java
@@ -0,0 +1,9 @@
+package com.example.MyBus_backand.service;
+
+/**
+ * @author Peter Huang
+ * @date 2022/11/23 15:27
+ * @detail 服务类基础接口，可以用于测试接口
+ **/
+public interface IBaseService {
+}
---
+++ MyBus_backand/src/main/java/com/example/MyBus_backand/service/ITeachersService.java
@@ -0,0 +1,10 @@
+package com.example.MyBus_backand.service;
+
+/**
+ * @author Peter Huang
+ * @date 2022/11/23 15:25
+ * @detail 演员类服务层接口
+ **/
+public interface ITeachersService {
+
+}
