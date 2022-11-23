a66c19a0f07c19e39c1a5d0e9e0b7574ff76e56f
Pt0819 <2471408797@qq.com>
2022-11-23 15:19:13 +0800

    Pt0819

---
+++ MyBus_backand/src/main/java/com/example/MyBus_backand/entity/Magnetic.java
@@ -0,0 +1,25 @@
+package com.example.MyBus_backand.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author Peter Huang
+ * @date 2022/11/23 15:09
+ * @detail 磁力链接实体类
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class Magnetic {
+    private String Magnetic_Name;           //电影磁力链接名称
+    private Integer Magnetic_Id;            //电影磁力链接Id
+    private String Magnetic_large;              //电影磁力链接大小（**GB）
+    private Date Magnetic_ReleaseDate;          //电影磁力链接分享时间
+
+}
